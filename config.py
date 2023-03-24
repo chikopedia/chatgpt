c5eb8a0c514388f6278ec52bd7a5218e1912120a
chikopedia <60287271+chikopedia@users.noreply.github.com>
2023-03-24 16:30:26 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+## CONFIGURATION FILE
+
+# Bot session name
+session_name_bot = "sessions/bot" 
+
+# Follow this guide to obtain your API credentials: 
+# https://core.telegram.org/api/obtaining_api_id
+API_ID = "24566921"
+API_HASH = "be5f63ba5553ad6286534a13cb6d0f56"
+
+# Write to @BotFather in Telegram to create a bot token
+BOT_TOKEN = "6296977664:AAEVGfXqNCIagYoJLymOEHf0fMdSB7KrEzM"
+
+# The ID of the OpenAI model to use for generating responses.
+model_engine = "gpt-3.5-turbo"
+
+# Get your API_KEY by creating an OpenAi account and the go here: 
+# https://platform.openai.com/account/api-keys
+openai_key = "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"
